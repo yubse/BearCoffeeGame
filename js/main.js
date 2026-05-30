be82bfdb09f46017068b5611bf1e9bf6a154eca7
yubse <1574813469@qq.com>
2026-05-30 15:02:31 +0800

    生日贺图加载不出来

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@
             uiCharSelect: './static/images/ui/char-select-bg.png',
             consoleShell: './static/images/ui/console-shell.png',
             controlGuide: './static/images/ui/control-guide.jpg',
-            birthdayCard: './static/images/birthday/panpan-birthday.png',
+            birthdayCard: './static/images/birthday/panpan-birthday.jpg',
             purchaseQrCard: './static/images/promo/purchase-qr.png',
 
             floor1Normal: './static/images/scenes/floor1.jpg',
