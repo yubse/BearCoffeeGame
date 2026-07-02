77fdc59d709f4fd88f8cd72151f25a2766bf81c7
yubse <1574813469@qq.com>
2026-07-02 15:59:11 +0800

    Update cafe map popup experience

---
+++ map/guangdong-cities-data.js
@@ -0,0 +1,43 @@
+﻿window.GUANGDONG_CITIES = {
+  "type": "FeatureCollection",
+  "name": "guangdong_cities",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "深圳",
+        "name_en": "Shenzhen",
+        "province": "广东"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [114.0579, 22.5431]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "惠州",
+        "name_en": "Huizhou",
+        "province": "广东"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [114.4152, 23.1115]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "广州",
+        "name_en": "Guangzhou",
+        "province": "广东"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [113.2644, 23.1291]
+      }
+    }
+  ]
+}
+;
